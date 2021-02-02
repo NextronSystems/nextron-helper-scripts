d1eaa614d6edc077b42390563c90639aa075a441
Florian Roth <florian.roth@nextron-systems.com>
2021-02-02 18:16:56 +0100

    refactor: refactored templates

--- thor-seed/thor-seed.ps1
+++ thor-seed/thor-seed.ps1
@@ -171,7 +171,7 @@ $PresetConfig_FullLookback = @"
 rebase-dir: $($OutputPath)  # Path to store all output files (default: script location)
 nosoft: true           # Don't trottle the scan, even on single core systems
 global-lookback: true  # Apply lookback to all possible modules
-lookback: 4            # Log and Eventlog look back time in days
+lookback: 14           # Log and Eventlog look back time in days
 # cpulimit: 70         # Limit the CPU usage of the scan
 sigma: true            # Activate Sigma scanning on Eventlogs
 nofserrors: true       # Don't print an error for non-existing directories selected in quick scan 
@@ -221,7 +221,7 @@ nothordb: true     # Don't create a local SQLite database for differential analy
 # SELECT YOU CONFIG
 # Select your preset config
 # Choose between: $PresetConfig_Full, $PresetConfig_Quick, $PresetConfig_FullLookback
-$PresetConfig = $PresetConfig_Full
+$PresetConfig = $PresetConfig_FullLookback
 
 # False Positive Filters
 $UseFalsePositiveFilters = $True
